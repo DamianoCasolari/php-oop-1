97154944a81965fdbb44fd0fd047b5449f9da780
DamianoCasolari <damianocasolari@gmail.com>
2023-05-16 17:30:29 +0200

    correct class names

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . './Models/movieClass.php';
+include __DIR__ . './Models/Movie.php';
 
 
 $matrix = new Movie('matrix', 'ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati ex veniam reprehenderit consequatur cupiditate, harum corporis fugit ducimus cum.', 2023, new Genre('fantasy'));
