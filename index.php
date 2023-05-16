9fcf50fe903607bc1caf431642932ead5068011d
DamianoCasolari <damianocasolari@gmail.com>
2023-05-16 15:37:02 +0200

    add second object

--- index.php
+++ index.php
@@ -40,8 +40,9 @@ class Movie
 }
 
 $matrix = new Movie('matrix', 'ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati ex veniam reprehenderit consequatur cupiditate, harum corporis fugit ducimus cum.', 2023, new Genre('fantasy'));
+$titanic = new Movie('Titanic', 'ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati ex veniam reprehenderit consequatur cupiditate, harum corporis fugit ducimus cum.', 1999, new Genre('romantic'));
 
-var_dump($matrix)
+var_dump($matrix, $titanic)
 
 
 
