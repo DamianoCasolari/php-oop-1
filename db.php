5bcd97090c42756ade7e311b9276fc2fc423bbe1
DamianoCasolari <damianocasolari@gmail.com>
2023-05-16 17:10:16 +0200

    add cards to insert data

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+include __DIR__ . './Models/movieClass.php';
+
+
+$matrix = new Movie('matrix', 'ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati ex veniam reprehenderit consequatur cupiditate, harum corporis fugit ducimus cum.', 2023, new Genre('fantasy'));
+$titanic = new Movie('Titanic', 'ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati ex veniam reprehenderit consequatur cupiditate, harum corporis fugit ducimus cum.', 1999, new Genre('romantic'));
+$Movies = [$matrix, $titanic];
+
+// var_dump($Movies[0])
+
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// include __DIR__ . './Models/movieClass.php';
+include __DIR__ . './db.php';
 
 ?>
 
@@ -33,12 +33,17 @@
 
 <body>
     <div class="my_container">
-        <div id="app">
+        <div id="app" class="d-flex flex-wrap">
 
-
-
-
-
+            <div class="card my_card bg-dark text-light">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    b5
+                </div>
+            </div>
 
         </div>
     </div>
